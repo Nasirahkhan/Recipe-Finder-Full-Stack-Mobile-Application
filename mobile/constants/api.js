9192e4f7b30267564340d0fc19df76b2fd63d6ac
Nasirah Khan <nasirahkhan01@gmail.com>
2026-01-10 01:45:58 +0500

    Initial commit - Recipe Finder

---
+++ mobile/constants/api.js
@@ -0,0 +1,4 @@
+// if you're using your physical phone, change this to the deployed url
+// https://recipe-app-api-expo.onrender.com
+//export const API_URL = "http://localhost:5001/api";
+export const API_URL = "https://recipe-app-api-expo.onrender.com/api";
